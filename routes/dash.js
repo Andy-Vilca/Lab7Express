684030cea591429121719ec59bc4580d39414815
diego12 <diego.anchillo@tecsup.edu.pe>
2020-05-19 12:31:56 -0500

    commit Diego-Dashboard ver.2

---
+++ routes/dash.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  datos={
+    "nombre":"DiegoAndyGianluca",
+    "email":"diego.andy.gianluca@gmail.com"
+  };
+  res.render('dash',function(err,html){
+    res.render('templates/layout',{
+      'usuario':datos,
+      'tituloSeccion':'Desarrolo de Aplicaciones Web',
+      'seccion':html
+    })
+  });
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  datos={
-    "nombre":"DiegoAndyGianluca",
-    "email":"diego.andy.gianluca@gmail.com"
-  };
-  res.render('dash',function(err,html){
-    res.render('templates/layout',{
-      'usuario':datos,
-      'tituloSeccion':'Desarrolo de Aplicaciones Web',
-      'seccion':html
-    })
-  });
+  res.render('login');
 });
 
 module.exports = router;
